5af01275b9c731314abf9bd2c40da0d4252781d0
Jingyun Bian <j4bian@uwaterloo.ca>
2016-03-13 15:18:29 -0400

    prob 204, 28

---
+++ leetcode/28.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int pos = haystack.find(needle);
+        if (pos == string::npos) return -1;
+        else return pos;
+    }
+};
