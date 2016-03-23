16a4bcb41d01af55da114d9a11a40f65c0a0d2a1
Jingyun Bian <j4bian@uwaterloo.ca>
2016-03-23 16:12:07 -0400

    prob 150

---
+++ leetcode/150.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) 
+    {
+        stack<int> s;
+        while (!s.empty()) s.pop();
+        
+        int a, b;
+        for (int i=0;i<tokens.size();++i)
+        {
+                if (tokens[i] == "+")
+                {
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a+b);
+                }
+                else if (tokens[i] == "-")
+                {
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a-b);
+                }
+                else if (tokens[i] == "*")
+                {
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a*b);
+                }
+                else if (tokens[i] == "/")
+                {
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a/b);
+                }
+                else
+                    s.push(stoi(tokens[i]));
+        }
+        return s.top();
+    }
+};
