6859b55cfaf68e45cc1e830fd05ae34158f0f79f
Jingyun Bian <j4bian@uwaterloo.ca>
2016-03-27 14:29:55 -0400

    prob 122

---
+++ leetcode/122.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) 
+    {
+        int n = p.size();
+        if (n == 0) return 0;
+        
+        int *busy = new int [n];
+        int *idle = new int [n];
+        
+        busy[0] = -p[0];
+        idle[0] = 0;
+        
+        for (int i=1;i<n;++i)
+        {
+            busy[i] = max(busy[i-1], idle[i-1] - p[i]);
+            idle[i] = max(idle[i-1], busy[i-1] + p[i]);
+        }
+        
+        return max(busy[n-1], idle[n-1]);
+    }
+};
