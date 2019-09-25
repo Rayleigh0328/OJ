e9de2225ca740c7c6de7788ebff14bdb2d38d0e1
Jingyun Bian <jingyun.bian@centro-T000325.centro.net>
2019-09-24 23:50:41 -0400

    categorize leetcode 331, tree traverse
    key observation: if null is printed, traverse order can define the
    orignal tree

---
+++ categorized/tree/leetcode_331.cpp
@@ -0,0 +1,1 @@
+../../leetcode/331.cpp
--- leetcode/331.cpp
+++ leetcode/331.cpp
@@ -1,37 +1,29 @@
 class Solution {
 private:
-    int **f;
     set<int> val_pos;
     
-    int solve(int start)
-    {
-        if (val_pos.find(start) == val_pos.end())
-            return start + 1;
+    int solve(int start) {
+        if (val_pos.find(start) == val_pos.end()) return start + 1;
         int right_start = solve(start+1);
         return solve(right_start);
     }
     
 public:
-    bool isValidSerialization(string st) 
-    {
+    bool isValidSerialization(string st) {
         val_pos.clear();
         
         int n = 0;
         int pos = 0;
-        while (pos < st.size())
-        {
-            if (st[pos] == ',') 
-            {
+        while (pos < st.size()) {
+            if (st[pos] == ',') {
                 ++n;
                 ++pos;
             }
             else
-                if (st[pos] == '#')
-                {
+                if (st[pos] == '#') {
                     ++pos;
                 }
-                else
-                {
+                else {
                     val_pos.insert(n);
                     while (isdigit(st[pos])) ++pos;
                 }
