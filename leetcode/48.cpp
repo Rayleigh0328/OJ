f1f6078801821891fd0355743dcc732833830359
Ray Bian <rayleigh0328@gmail.com>
2019-10-25 13:16:23 -0400

    update solution for 48

--- leetcode/48.cpp
+++ leetcode/48.cpp
@@ -1,34 +1,28 @@
+// rotation matrix 
 class Solution {
-private:
-    void solve(int xs, int ys, int step, vector<vector<int>> &matrix)
-    {
-        int x0,y0, x1, y1, x2, y2, x3, y3;
-        for (int i=0;i<step;++i)
-        {
-            x0 = xs;
-            y0 = ys + i;
-            
-            x1 = xs + i;
-            y1 = ys + step; 
-            
-            x2 = xs + step;
-            y2 = ys + step - i;
-            
-            x3 = xs + step - i;
-            y3 = ys;
-            
-            int tmp = matrix[x3][y3];
-            matrix[x3][y3] = matrix[x2][y2];
-            matrix[x2][y2] = matrix[x1][y1];
-            matrix[x1][y1] = matrix[x0][y0];
-            matrix[x0][y0] = tmp;
-        }
+    
+    void rotate(vector<vector<int>>& a, int i, int j){
+        int m = a.size() - 1;
+        int tmp = a[m-j][i];
+        a[m-j][i] = a[m-i][m-j];
+        a[m-i][m-j] = a[j][m-i];
+        a[j][m-i] = a[i][j];
+        a[i][j] = tmp;
     }
+    
 public:
-    void rotate(vector<vector<int>>& matrix) 
-    {
-        int len = matrix.size();
-        for (int i=0;i<= len/2; ++i)
-            solve(i,i,len-1-2*i,matrix);
+    void rotate(vector<vector<int>>& a) {
+        // (i,j) --> (j,n-1-i) --> (n-1-i, n-1-j) --> (n-1-j, i)
+        int n = a.size();
+        if (n & 1){
+            for (int i=0;i<(n-1)/2;++i)
+                for (int j=0;j<(n+1)/2;++j)
+                    rotate(a,i,j);
+        }
+        else{
+            for (int i=0;i<n/2;++i)
+                for (int j=0;j<n/2;++j)
+                    rotate(a,i,j);
+        }
     }
 };
