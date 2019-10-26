1c38941a44a24b8686c398383cefa6b6253d1925
Ray Bian <rayleigh0328@gmail.com>
2019-10-26 19:15:29 -0400

    add 97, dp

---
+++ categorized/dp/leetcode_97.cpp
@@ -0,0 +1,1 @@
+../../leetcode/97.cpp
--- leetcode/97.cpp
+++ leetcode/97.cpp
@@ -1,44 +1,21 @@
 class Solution {
-private:
-    typedef pair<int, int> pos_ind;
     
-    string x,y,z;
-    set<pos_ind> set1;
-    set<pos_ind> set2;
-    
-    void solve(int z_pos, set<pos_ind>& prev, set<pos_ind>&next)
-    {
-        next.clear();
-        for (set<pos_ind>::iterator it = prev.begin(); it != prev.end(); ++it)
-        {
-            int x_pos = it->first;
-            int y_pos = it->second;
-            
-            if (x_pos + 1 < x.size() && x[x_pos+1] == z[z_pos]) next.insert(pos_ind(x_pos+1, y_pos));
-            if (y_pos + 1 < y.size() && y[y_pos+1] == z[z_pos]) next.insert(pos_ind(x_pos, y_pos+1));
-        }
+    bool solve(const string& s1, int i, const string &s2, int j, const string &s3, vector<vector<int>>& f){
+        if (i < 0 || j < 0) return false;
+        if (f[i][j] != -1) return f[i][j];
+        bool result = false;
+        if (i>0 && s3[i+j-1] == s1[i-1]) result = result || solve(s1,i-1,s2,j,s3,f);
+        if (j>0 && s3[i+j-1] == s2[j-1]) result = result || solve(s1,i,s2,j-1,s3,f);
+        return (f[i][j] = result);
     }
     
 public:
-    bool isInterleave(string s1, string s2, string s3) 
-    {
-        x = s1;
-        y = s2;
-        z = s3;
-        if (z.size() != x.size() + y.size()) return false;
-        set2.clear();
-        set1.clear();    
-        set1.insert(pos_ind(-1,-1));
-        
-        for (int i=0;i<s3.size();++i)
-            if (i % 2 == 0) 
-                solve(i,set1, set2);
-            else 
-                solve(i,set2, set1);
-     
-        if (s3.size() % 2 == 0)
-            return !set1.empty();
-        else 
-            return !set2.empty();
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.length();
+        int m = s2.length();
+        if (s3.length() != n + m) return false;
+        vector<vector<int>> f(n+1, vector<int>(m+1, -1));
+        f[0][0] = 1;
+        return solve(s1,n, s2,m, s3, f);
     }
 };
