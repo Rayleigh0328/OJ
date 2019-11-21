a20fdcf2d9f11eb292993a05439e4e22d0e7d459
Ray Bian <rayleigh0328@gmail.com>
2019-11-21 15:32:21 -0500

    add 441, math

---
+++ leetcode/441.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    const double eps = 1e-8;
+public:
+    int arrangeCoins(int m) {
+        long long n = m;
+        int x = (sqrt(8*n+1) - 1) / 2.0 + eps;
+        return x;
+    }
+};
