7e20692b3b2c54e19d360b93cfe553a62a44f113
Jingyun Bian <jingbian@amazon.com>
2020-02-24 13:05:39 -0500

    update 1289 solution
    record smallest / second smallest element position from previous row

--- leetcode/1289.cpp
+++ leetcode/1289.cpp
@@ -7,11 +7,18 @@ public:
         int m = a[0].size();
         vector<vector<int>> f(n, vector<int>(m,inf));
         for (int j=0;j<m;++j) f[0][j] = a[0][j];
-        for (int i=1;i<n;++i)
-            for (int j=0;j<m;++j)
-                for (int k=0;k<m;++k)
-                    if (k != j) 
-                        f[i][j] = min(f[i][j], f[i-1][k] + a[i][j]);
+        for (int i=1;i<n;++i){
+            vector<int> v;
+            for (int j=0;j<m;++j) v.push_back(j);
+            sort(v.begin(), v.end(), [&f, &i](const int &x,const int &y){return f[i-1][x] < f[i-1][y];});
+
+
+            for (int j=0;j<m;++j){
+                if (j==v[0]) f[i][j] = f[i-1][v[1]] + a[i][j];
+                else f[i][j] = f[i-1][v[0]] + a[i][j];
+            }
+        }
+
         int result = inf;
         for (int j=0;j<m;++j)
             result = min(result, f[n-1][j]);
